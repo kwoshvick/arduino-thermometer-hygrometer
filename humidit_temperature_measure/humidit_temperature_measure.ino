a20c0774546223ea0785517293bfe718d64af785
kwoshvick <kwoshvick@gmail.com>
2018-04-15 13:05:19 +0300

    code refactor

--- humidit_temperature_measure/humidit_temperature_measure.ino
+++ humidit_temperature_measure/humidit_temperature_measure.ino
@@ -1,55 +1,60 @@
 #include "DHT.h"
-
+#include <WiFi.h>
+#include <HTTPClient.h>
 #define DHTPIN 13     // what digital pin we're connected to
-
 #define DHTTYPE DHT11   // DHT 11
 
+// Wifi credentials
+const char* ssid = "xxxxxxxxxxx";
+const char* password =  "xxxxxxxxxx";
 
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+HTTPClient http;
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
   Serial.println("DHT ....");
-
   dht.begin();
+//  initialize wifi
+  WiFi.begin(ssid, password);  
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  } 
+  Serial.println("Connected to the WiFi network"); 
+}
+
+// send the details to the server
+void send_to_server(float temperature, float humidity,float heat_index){
+  // print to serial monitor console
+  Serial.println("Temperature "+String(temperature)+ " Humidity "+String(humidity)+ " Heat Index "+String(heat_index));
+//  http.begin("http://3388653d.ngrok.io/iot/crud/alert/1/0"); 
+//  http.addHeader("Content-Type", "text/plain");             //Specify content-type header 
+//  http.POST("POSTING from ESP32"); 
 }
 
 void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
-
+  
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+  float humidity = dht.readHumidity();
+  
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  float temperature = dht.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
   // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
+  float heat_index = dht.computeHeatIndex(temperature, humidity, false);
 
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  // call method to send details to server
+  send_to_server(temperature,humidity,heat_index);
 }
